d69a5aa59423e06bd4f76bd459603c7b045de46f
Petrrixx <bolecek@stud.uniza.sk>
2024-10-20 22:12:33 +0200

    style.css update a úvodná stránka

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -6,9 +6,20 @@
 
 @section('content')
 
-    <div>
+    <div class="background-image" style="background-image: url('{{ asset('images/background2.JPEG') }}');">
+        <a href="{{ url('/Contact') }}" class="clickable-image left">
+            <img src="{{ asset('images/location.png') }}" alt="Kontakt" />
+        </a>
+        <a href="{{ url('/DailyMenu') }}" class="clickable-image right">
+            <img src="{{ asset('images/menu.png') }}" alt="Denné menu" />
+        </a>
+    </div>
+
+
+    {{-- Obsah pod pozadím --}}
+    <div class="content">
         <h1>Vitajte na stránke Gazdovského dvora!</h1>
-        <p>Toto je úvodná stránka s vlastným obsahom.</p>
+        <p>Toto je úvodná stránka.</p>
     </div>
 
 @endsection
